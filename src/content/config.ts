6e8e29a1446c4d31b56b9287cf44cba15e60aba4
Jérémy SERSOUB <116919971+jeremsers@users.noreply.github.com>
2023-12-17 06:37:51 +0100

    modified layout

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,29 @@
 import { defineCollection, z } from "astro:content";
 
 const projetCollection = defineCollection({
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    img: image().refine((img) => img.width >= 0, {
-      message: "Cover image must be at least 300 pixels wide!",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      img: image().refine((img) => img.width >= 0, {
+        message: "Cover image must be at least 300 pixels wide!",
+      }),
+      description: z.string(),
     }),
-    description: z.string()
-    
-  }),
 });
 
 const projectlistCollection = defineCollection({
-    type: 'data',
-    schema: ({ image }) => z.object ({
-        titre: z.string(),
-        image: image().refine((img) => img.width >= 0, {
-            message: "Cover image must be at least 300 pixels wide!",
-          }),
-          description: z.string()
-
-    })
-
-})
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      titre: z.string(),
+      image: image().refine((img) => img.width >= 0, {
+        message: "Cover image must be at least 300 pixels wide!",
+      }),
+      description: z.string(),
+    }),
+});
 
 export const collections = {
   projet: projetCollection,
-  projectlist: projectlistCollection
+  projectlist: projectlistCollection,
 };
